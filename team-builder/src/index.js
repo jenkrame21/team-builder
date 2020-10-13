ede61075bb347f3ef32dbf748ef910ef66d837fe
Jennifer Kramer <jenkrame@gmail.com>
2020-10-13 18:03:47 -0400

    Working on index.js file.

---
+++ team-builder/src/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const usersList = [
+  { firstName: 'Jennifer', lastName: 'Kramer', course: 'Web Development'}
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
